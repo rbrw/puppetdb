f735be4f747d2121e2e32c77f2e194615261618e
Rob Browning <rlb@puppetlabs.com>
2015-01-23 18:53:01 -0600

    (PDB-1102) Upgrade to activemq 5.10.1

--- project.clj
+++ project.clj
@@ -58,9 +58,14 @@
                  [org.hsqldb/hsqldb "2.2.8"]
                  [org.postgresql/postgresql "9.2-1003-jdbc4"]
                  [clojureql "1.0.3"]
+
                  ;; MQ connectivity
-                 [clamq/clamq-activemq "0.4" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.activemq/activemq-core "5.7.0" :exclusions [org.slf4j/slf4j-api org.fusesource.fuse-extra/fusemq-leveldb]]
+                 [org.apache.activemq/activemq-broker "5.10.1"]
+                 [org.apache.activemq/activemq-kahadb-store "5.10.1"]
+                 [clamq/clamq-activemq "0.4"
+                  :exclusions [org.slf4j/slf4j-api
+                               org.apache.activemq/activemq-core]]
+
                  ;; bridge to allow some spring/activemq stuff to log over slf4j
                  [org.slf4j/jcl-over-slf4j "1.7.6"]
                  ;; WebAPI support libraries.
