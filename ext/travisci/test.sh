4662d4e18da0dc9d115d293c4808b6952078560b
Rob Browning <rlb@puppet.com>
2017-05-18 13:04:57 -0500

    Try pg 10

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -13,6 +13,14 @@ run-unit-tests()
   pgdir="$(pwd)/test-resources/var/pg"
   readonly pgdir
 
+  if test "$PDB_TEST_PG" = 10; then
+      sudo -i apt-get purge postgresql-9.6 postgresql-9.6-client postgresql-contrib-9.6
+
+      sudo -i 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+      sudo -i apt-get install -y --purge \
+           postgresql-10 postgresql-10-client postgresql-contrib-10
+  fi
+
   export PGHOST=127.0.0.1
   export PGPORT=5432
   ext/bin/setup-pdb-pg "$pgdir"
