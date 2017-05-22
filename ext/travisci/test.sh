c728968ca043c72647a60326d474b9a00e9ae44f
Rob Browning <rlb@puppet.com>
2017-05-22 15:46:28 -0500

    Try pg 10

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -11,14 +11,28 @@ hostname --fqdn
 run-unit-tests()
 (
   pgdir="$(pwd)/test-resources/var/pg"
-  readonly pgdir
 
   if test "$PDB_TEST_PG" = 10; then
-      sudo -i apt-get purge postgresql-9.6 postgresql-9.6-client postgresql-contrib-9.6
 
-      sudo -i 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+      for minor in {1..6}; do
+        #sudo -i service postgresql stop "9.$minor"
+        sudo -i apt-get remove -ym postgresql-"9.$minor"
+      done
+
+      sudo -i service postgresql stop
+      ps aux | grep postgres
+      ls -l /var/lib/dpkg/info/*postgres*.list
+
+      sudo -i apt-get install wget ca-certificates
+      echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main 10" \
+           | sudo -i bash -c 'cat > /etc/apt/sources.list.d/pgdg.list'
+      wget --quiet -O - 'https://www.postgresql.org/media/keys/ACCC4CF8.asc' \
+          | sudo -i apt-key add -
+      sudo -i apt-get update
+      ps aux | grep postgres
       sudo -i apt-get install -y --purge \
-           postgresql-10 postgresql-10-client postgresql-contrib-10
+           postgresql-10 postgresql-client-10 postgresql-contrib-10
+      sudo -i service postgresql start 10
   fi
 
   export PGHOST=127.0.0.1
@@ -26,6 +40,9 @@ run-unit-tests()
   ext/bin/setup-pdb-pg "$pgdir"
   ext/bin/pdb-test-env "$pgdir" lein test "$PDB_TEST_SELECTOR"
 )
+
+echo FINALLY TESTING
+date
 
 case "$PDB_TEST_LANG" in
   clojure)
